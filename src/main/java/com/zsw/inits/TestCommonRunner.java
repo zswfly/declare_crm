078f8cd225320462acc08c5fb8fd6d4f7b025624
zhangshaowei <zhangshaowei@iplas.com.cn>
2020-04-11 20:49:40 +0800

    new

---
+++ src/main/java/com/zsw/inits/TestCommonRunner.java
@@ -0,0 +1,20 @@
+package com.zsw.inits;
+
+import com.zsw.utils.TestUtil;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by zhangshaowei on 2020/4/11.
+ */
+
+@Component
+@Order(1)
+public class TestCommonRunner  implements CommandLineRunner{
+    @Override
+    public void run(String... args) throws Exception {
+        TestUtil testUtils = new  TestUtil();
+        testUtils.test();
+    }
+}
